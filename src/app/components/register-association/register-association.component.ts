19d68286be28597c98846db02efea4588a0fdef7
Óscar Arroyo Aguadero <oscararroyoaguadero.guadalupe@alumnado.fundacionloyola.net>
2024-04-23 19:15:50 +0200

    CREATED: Creacion del componente de registro de asociaciones

---
+++ src/app/components/register-association/register-association.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-association',
+  templateUrl: './register-association.component.html',
+  styleUrls: ['./register-association.component.css']
+})
+export class RegisterAssociationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
