0a0334864137f47e3cf82bcc426aa45b24ed18a7
CodeLandscape <ltabareslozano.guadalupe@alumnado.fundacionloyola.net>
2024-04-25 21:19:39 +0200

    CREATE:login implementado

--- src/app/models/usuario.ts
+++ src/app/models/usuario.ts
@@ -1,17 +1,26 @@
-class Usuario {
+export class Usuario {
   id: number;
   correo: string;
   password: string;
   direccion: string;
   poblacion: string;
-  idProvincia: Provincia;
-
-  constructor(id: number, correo: string, password: string, direccion: string, poblacion: string, idProvincia: Provincia) {
+  tlf: string;
+  activo: boolean;
+  provincia: Provincia;
+  // @ts-ignore
+  rol: Rol;
+  // tslint:disable-next-line:max-line-length
+  // @ts-ignore
+  // tslint:disable-next-line:max-line-length
+  constructor(id: number, correo: string, password: string, direccion: string, poblacion: string, tlf: string, activo: boolean, provincia: Provincia, rol: Rol) {
     this.id = id;
     this.correo = correo;
     this.password = password;
     this.direccion = direccion;
     this.poblacion = poblacion;
-    this.idProvincia = idProvincia;
+    this.tlf = tlf;
+    this.activo = activo;
+    this.provincia = provincia;
+    this.rol = rol;
   }
 }
---
+++ src/app/services/usuarios-services.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Usuario} from '../models/usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuariosServicesService {
+
+  constructor(private http: HttpClient) { }
+
+  getUsuarios() {
+    return this.http.get<Usuario[]>('http://localhost:8080/api-backend/usuario/all');
+  }
+}
