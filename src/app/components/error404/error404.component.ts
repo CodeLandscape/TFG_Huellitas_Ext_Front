5c13a74af2470d8ba0002b5e17d30eb69ca6a318
CodeLandscape <ltabareslozano.guadalupe@alumnado.fundacionloyola.net>
2024-05-30 17:11:51 +0200

    FIX: implementado boton de volver tras en el error 404 component

--- src/app/components/error404/error404.component.ts
+++ src/app/components/error404/error404.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-error404',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Error404Component implements OnInit {
 
-  constructor() { }
+  constructor(private location: Location) { }
 
   ngOnInit(): void {
+  }
+
+  goBack() {
+    this.location.back();
   }
 
 }
